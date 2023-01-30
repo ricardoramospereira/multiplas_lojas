662d032fa56246a90f83431f27c168397cfe5b14
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-01-30 08:07:21 -0300

    area_admin

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from .juzmin import JAZZMIN_SETTINGS
+
+# from .admin_custom import JAZZMIN_SETTINGS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -129,3 +133,6 @@ STATIC_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# alteração area admin
+JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
